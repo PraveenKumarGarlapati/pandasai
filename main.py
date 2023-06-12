5b8b5cee8a6ca379a9dac1d7e47742274235f776
Praveen106 Kumar <prvn012@gmail.com>
2023-06-12 19:15:37 +0530

    removedApi

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pandasai import PandasAI
 # from pandasai.llm.openai import OpenAI
 # from settings import API
 import streamlit as st
+import matplotlib as plt
 
 
 
@@ -13,24 +14,25 @@ df = pd.read_excel(f'data/{path}')
 #Check
 
 
-
 def main():
     st.title("Generate Insights")
     
     # Prompt input field
+    API = st.text_input("Provide your API Key here")
     prompt = st.text_input("Enter a text:")
     print(prompt)
     
     # OpenAI
     from pandasai.llm.openai import OpenAI
-    llm = OpenAI(api_token=API)
-
-    pandas_ai = PandasAI(llm)
-    # prompt_here = prompt
     # print(pandas_ai.run(df, prompt=prompt_here))
 
     if st.button("Show Insights"):
-      st.write(pandas_ai.run(df, prompt=prompt))
+        llm = OpenAI(api_token=API)
+
+        pandas_ai = PandasAI(llm, verbose= True)
+        # prompt_here = prompt
+
+        st.write(pandas_ai.run(df, prompt=prompt))
 
 
 if __name__ == "__main__":
