18d3aa884919f7d9b7af668aed903fd5e5b4e8f5
Praveen106 Kumar <prvn012@gmail.com>
2023-06-13 09:17:44 +0530

    added_seaborn

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ from pandasai import PandasAI
 # from settings import API
 import streamlit as st
 import matplotlib as plt
+import tkinter
 
 
 
-path = 'Sample_NMReport.xlsx'
-df = pd.read_excel(f'data/{path}')
+# path = 'Sample_NMReport.xlsx'
+# df = pd.read_excel(f'data/{path}')
+# prompt = 'Show a histogram please'
 
-#Check
-
+# #Check
+# API = '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'
 
 def main():
+    st.title("Upload and Display DataFrame")
+
+    # Upload option and button
+    uploaded_file = st.file_uploader("Upload CSV file", type="csv")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.write("Top", 5, "rows of the dataframe:")
+        st.dataframe(df.head(5))
+
     st.title("Generate Insights")
     
     # Prompt input field
@@ -24,7 +35,7 @@ def main():
     
     # OpenAI
     from pandasai.llm.openai import OpenAI
-    # print(pandas_ai.run(df, prompt=prompt_here))
+
 
     if st.button("Show Insights"):
         llm = OpenAI(api_token=API)
